8226b9a5c63c1f191be50ded5ab3065e772cb589
Jeremías Pérez <jeremiasperezcontell@gmail.com>
2025-06-19 16:35:04 +0200

    fixed connection closing

--- OCPPServer.js
+++ OCPPServer.js
@@ -6,12 +6,10 @@ class OCPPServer {
     constructor() {
         this._server = new RPCServer({
             protocols: ['ocpp1.6'], // server accepts ocpp1.6 subprotocol
-            strictMode: true,       // enable strict validation of requests & responses
+            strictMode: false,       // enable strict validation of requests & responses
         })
 
-        this._clients = []
-        this._lastTransactionId = 0;
-        this._currentTransactionIdByClient = {}
+        this._clients = {}
     }
     async init() {
         let that = this
@@ -24,14 +22,30 @@ class OCPPServer {
         })
 
         this._server.on('client', async (client) => {
-            this._clients.push(client)
-            this._currentTransactionIdByClient[client.identity] = {}
-
             console.log(`${client.session.sessionId} connected!`) // `XYZ123 connected!`
+
 
             // create a specific handler for handling BootNotification requests
             client.handle('BootNotification', ({params}) => {
+                if (client.identity == null) return
                 console.log(`Server got BootNotification from ${client.identity}:`, params)
+
+                if (this._clients[client.identity] != null){
+                    return {
+                        status: "Rejected"
+                    }
+                }
+                this._clients[client.identity] = {client: client, connectors: {}, lastTransactionId: 1}
+
+                client.on('close', (e) => {
+                    console.log("client closed")
+                    delete this._clients[client.identity]
+                })
+
+                client.on('disconnect', () => {
+                    console.log("client disconnected")
+                    delete this._clients[client.identity]
+                })
 
                 // respond to accept the client
                 return {
@@ -54,15 +68,25 @@ class OCPPServer {
             // create a specific handler for handling StatusNotification requests
             client.handle('StatusNotification', ({params}) => {
                 console.log(`Server got StatusNotification from ${client.identity}:`, params)
-                return {}
             })
 
             // create a specific handler for handling StartTransaction requests
             client.handle('StartTransaction', ({params}) => {
                 console.log(`Server got StartTransaction from ${client.identity}:`, params)
-                let transactionId = that._lastTransactionId
-                this._currentTransactionIdByClient[client.identity][params.connectorId] = transactionId
-                that._lastTransactionId++
+                const cli = this._clients[client.identity]
+
+                /*const transactionId = cli.transactions.reduce((t,v) => Math.max(t.id, v), 0) + 1
+                cli.transactions.push({id: transactionId, connectorId: params.connectorId, status: 'Accepted'})
+
+                this._currentTransactionIdByClient[client.identity][params.connectorId] = transactionId*/
+                const connector = cli.connectors[params.connectorId]
+                let transactionId = cli.lastTransactionId
+                if(connector == null){
+                    cli.connectors[params.connectorId] = {activeTransaction: transactionId}
+                } else {
+                    connector.activeTransaction = transactionId
+                }
+                cli.lastTransactionId++
                 return {
                     transactionId: transactionId,
                     idTagInfo: {
@@ -94,37 +118,47 @@ class OCPPServer {
         console.log("OCPP Server started!")
     }
     async remoteStartTransaction(identity, idTag, connectorId) {
-        console.log(`send remote start to ${identity} in tag ${idTag} to connector ${connectorId}`)
-        const cli = this._clients.find((c) => c.identity === identity)
+        const cli = this._clients[identity]
         if(cli == null){
             console.log('Charger not found')
             return {status: 'error', message: 'charger not found'}
         }
+        const connector = cli.connectors[connectorId]
+        if(connector == null){
+            cli.connectors[connectorId] = {activeTransaction: null}
+        }
+
         try{
-            let response = await cli.call('RemoteStartTransaction',{idTag: idTag, connectorId: connectorId})
+            console.log(`sending remote start to ${identity} in tag ${idTag} to connector ${connectorId}`)
+            let response = await cli.client.call('RemoteStartTransaction',{idTag: idTag, connectorId: connectorId})
             console.log(response)
             if(response.status != null || response.status == 'Accepted') return {status: 'ok'}
             else return {status: 'error', message: 'Charger refused transaction'}
         } catch(error){
             return {status: 'error', message: error.message}
         }
-
     }
     async remoteStopTransaction(identity, connectorId) {
-        const cli = this._clients.find((c) => c.identity === identity)
+        const cli = this._clients[identity]
         if(cli == null){
             console.log('Charger not found')
             return {status: 'error', message: 'charger not found'}
         }
-        let transactionId = this._currentTransactionIdByClient[cli.identity][connectorId]
+        const connector = cli.connectors[connectorId]
+        if(connector == null){
+            console.log('Connector not found')
+            return {status: 'error', message: 'connector not found'}
+        }
+        const transactionId = connector.activeTransaction
         if (transactionId == null){
             return {status: 'error', message: 'No active transaction in charger'}
         }
+
         try{
             console.log(`send remote stop to ${identity} of transaction ${transactionId}`)
-            let response = await cli.call('RemoteStopTransaction',{transactionId: transactionId})
+            let response = await cli.client.call('RemoteStopTransaction',{transactionId: transactionId})
             if(response.status == 'Accepted'){
-                delete this._currentTransactionIdByClient[cli.identity][connectorId]
+                connector.activeTransaction = null
                 return {status: 'ok'}
             }
             else return {status: 'error', message: 'Charger rejected the operation'}
--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 5000
 
 const OCPPServer = require('./OCPPServer')
 
 const server = new OCPPServer()
-
 server.init().then(() => {
   app.post('/start', async (req, res) => {
     console.log("start received")
-    let response = await server.remoteStartTransaction('EVB-P20261797','abc',1)
+    const chargePointId = req.body?.chargePointId || 'EVB-P20261797'
+    const connectorId = req.body?.connectorId || 1
+    const tag = req.body?.tag || 'abc'
+    let response = await server.remoteStartTransaction(chargePointId,tag,connectorId)
     res.json(response)
   })
 
   app.post('/stop', async (req, res) => {
-    let response = await server.remoteStopTransaction('EVB-P20261797',1)
+    const chargePointId = req.body?.chargePointId || 'EVB-P20261797'
+    const connectorId = req.body?.connectorId || 1
+    let response = await server.remoteStopTransaction(chargePointId,connectorId)
     res.json(response)
   })
 
   app.listen(port, () => {
     console.log(`HTTP Server listening on port ${port}`)
   })
-
 })
+
+
+
 
